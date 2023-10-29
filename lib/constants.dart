653a944905e0c261bb851ad9d64bc6e5e1bc020e
mahmoodhamdi <mahmouashwah@gmail.com>
2023-10-29 06:38:26 +0200

    fonts and main packages

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = const Color(0xff115980);
+Color kSecondaryColor = const Color(0xffEADBC8);
+Color kThirdColor = const Color(0xffF8F0E5);
